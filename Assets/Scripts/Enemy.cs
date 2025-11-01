8ba9bd0cab7f65111ab70d842fb01eb112ccce15
Itai Galipoliti <itai.galipo@gmail.com>
2025-11-01 12:40:25 +0200

    Attack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,9 +16,12 @@ public class Enemy : MonoBehaviour
     public Transform player;
     public float chaseSpeed = 2f;
     public float retriveDistance =3f;
+
+    private Animator animator;
     void Start()
     {
         facingLeft = true;
+        animator = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,14 +32,28 @@ public class Enemy : MonoBehaviour
 
         if (collInfo == true)
         {
-            Vector2 targetPos = new Vector2(player.position.x, transform.position.y);
+
+            if (player.position.x > transform.position.x && facingLeft )
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+                facingLeft = false;
+            }
+            else if(player.position.x < transform.position.x && facingLeft==false)
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                facingLeft = true;
+            }
+
+                Vector2 targetPos = new Vector2(player.position.x, transform.position.y);
 
             if (Vector2.Distance(transform.position, targetPos) > retriveDistance)
             {
+                animator.SetBool("Attack",false);
                 transform.position = Vector2.MoveTowards(transform.position, targetPos, chaseSpeed * Time.deltaTime);
             }
             else
             {
+                animator.SetBool("Attack",true);
 
             }
 
