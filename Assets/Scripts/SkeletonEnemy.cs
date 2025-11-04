c2750e066344f8536d73d0b0150158a6234d95f7
Itai Galipoliti <itai.galipo@gmail.com>
2025-11-04 14:41:12 +0200

    skeleton enemy

---
+++ Assets/Scripts/SkeletonEnemy.cs
@@ -0,0 +1,176 @@
+using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+
+public class SkeletonEnemy : MonoBehaviour
+{
+    public int maxHealth = 3;
+
+    [Header("Movement Settings")]
+    public float walkSpeed = 1.5f;
+    public float chaseSpeed = 2f;
+
+    [Header("Detection Settings")]
+    public float attackRangeRadius = 6f;  // when to start chasing
+    public float retrieveDistance = 3f;   // when to start attacking
+    public Transform player;
+    public LayerMask whatIsPlayer;
+
+    [Header("Attack Settings")]
+    public Transform attackPoint;
+    public float attackRadius = 1.5f;
+
+    [Header("Other Settings")]
+    public GameObject floatingTextPrefab;
+    public Transform textSpawnPoint;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
+    private bool facingLeft = true;
+    private bool hasShaken = false;
+
+    private enum EnemyState { Idle, Chase, Attack }
+    private EnemyState currentState = EnemyState.Idle;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (player == null)
+        {
+            animator.SetFloat("Run", 0f);
+            animator.SetBool("Attack", false);
+            return;
+        }
+
+        if (maxHealth <= 0)
+        {
+            Die();
+            return;
+        }
+
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        // --- STATE CONTROL ---
+        if (distanceToPlayer <= retrieveDistance)
+            currentState = EnemyState.Attack;
+        else if (distanceToPlayer <= attackRangeRadius)
+            currentState = EnemyState.Chase;
+        else
+            currentState = EnemyState.Idle;
+
+        // --- STATE BEHAVIOR ---
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Chase:
+                ChasePlayer();
+                break;
+            case EnemyState.Attack:
+                AttackPlayer();
+                break;
+        }
+    }
+
+    // ---------------- STATES ----------------
+    void Idle()
+    {
+        animator.SetFloat("Walk", 0f);
+        animator.SetBool("Attack", false);
+    }
+
+    void ChasePlayer()
+    {
+        animator.SetBool("Attack", false);
+
+        // Face player
+        if (player.position.x > transform.position.x && facingLeft)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingLeft = false;
+        }
+        else if (player.position.x < transform.position.x && !facingLeft)
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            facingLeft = true;
+        }
+
+        // Move toward player
+        Vector2 targetPos = new Vector2(player.position.x, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, chaseSpeed * Time.deltaTime);
+
+        // Play walk animation like player
+        animator.SetFloat("Walk", 1f);
+    }
+
+    void AttackPlayer()
+    {
+        animator.SetFloat("Run", 0f);
+        animator.SetBool("Attack", true);
+       
+    }
+
+    // ---------------- ATTACK LOGIC ----------------
+    public void Attack()
+    {
+        Collider2D collInfo = Physics2D.OverlapCircle(attackPoint.position, attackRadius, whatIsPlayer);
+        if (collInfo && collInfo.gameObject.GetComponent<Player>() != null)
+        {
+            Player.Instance.TakeDamage(1);
+        }
+    }
+
+    // ---------------- DAMAGE / DEATH ----------------
+    public void TakeDamage(int damageAmount)
+    {
+        if (maxHealth <= 0)
+            return;
+
+        maxHealth -= damageAmount;
+        animator.SetTrigger("hit");
+        CameraShake.instance.Shake(2.5f, .15f);
+
+        Instantiate(floatingTextPrefab, textSpawnPoint.position, Quaternion.identity);
+    }
+
+    void Die()
+    {
+        animator.SetBool("Death", true);
+        rb.gravityScale = 0f;
+        boxCollider2D.enabled = false;
+
+        if (!hasShaken)
+        {
+            hasShaken = true;
+            CameraShake.instance.Shake(2f, .12f);
+        }
+
+        Destroy(gameObject, 5f);
+    }
+
+    // ---------------- DEBUG ----------------
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, attackRangeRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Arrow"))
+        {
+            TakeDamage(1);
+            Destroy(other.gameObject);
+        }
+    }
+}
